d78c4106ed08dea5d05bd2d3daa88cd78bfb1d08
wuatiwuati <92826456+wuatiwuati@users.noreply.github.com>
2021-10-19 15:59:36 -0500

    Add files via upload

---
+++ Sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  var canvas=createCanvas(640, 480);
+  //canvas.parent('lienzo'):
+   background(0);
+}
+
+
+function draw() {
+fill(random(255),random(255), random (255));
+  textSize(random(30));
+  text("JAVA SCRIPT",random(640), random(480));
+  
+  
+}
+function mouseClicked(){
+  background(0);
+}
